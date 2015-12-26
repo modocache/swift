12a1f5de1c35e53085960489b610a84b6a7511aa
Brian Gesiak <modocache@gmail.com>
2015-12-26 03:49:28 -0500

    [XCTest] A better smoke test
    Can't figure out why this isn't working.
    
    Output:
    
    ```
    [ RUN      ] XCTest.XCTAssertTrue
    out>>> check failed at /Users/bgesiak/GitHub/apple/swift/validation-test/stdlib/XCTest.swift, line 27
    out>>> expected: 1 (of type Swift.UInt)
    out>>> actual: 0 (of type Swift.UInt)
    out>>> check failed at /Users/bgesiak/GitHub/apple/swift/validation-test/stdlib/XCTest.swift, line 29
    out>>> expected: 1 (of type Swift.UInt)
    out>>> actual: 0 (of type Swift.UInt)
    out>>> check failed at /Users/bgesiak/GitHub/apple/swift/validation-test/stdlib/XCTest.swift, line 30
    out>>> expected: false
    [     FAIL ] XCTest.XCTAssertTrue
    XCTest: Some tests failed, aborting
    UXPASS: []
    FAIL: ["XCTAssertTrue"]
    SKIP: []
    ```
    
    In other words, it looks like:
    
    - `failureCount == 0`
    - `totalFailureCount == 0`
    - `hasSucceeded == true`
    
    Which doesn't make sense, since the test case contains a single
    statement that asserts `1 == 2`. What am I missing?

--- validation-test/stdlib/XCTest.swift
+++ validation-test/stdlib/XCTest.swift
@@ -1,4 +1,4 @@
-// RUN: %target-build-swift -module-name main %s
+// RUN: %target-run-stdlib-swift
 // REQUIRES: executable_test
 
 // REQUIRES: objc_interop
@@ -6,10 +6,29 @@
 // watchOS 2.0 does not have a public XCTest module.
 // XFAIL: OS=watchos
 
+import StdlibUnittest
 import XCTest
 
-func smokeTest() {
-  var subject: AnyObject? = nil
-  XCTAssertNil(subject)
+var XCTestTestSuite = TestSuite("XCTest")
+
+XCTestTestSuite.test("XCTAssertTrue") {
+  class MyTestCase: XCTestCase {
+    func test_myThing() {
+      XCTAssertEqual(1, 2)
+    }
+  }
+
+  let testCase = MyTestCase(selector: "test_myThing")
+  testCase.runTest()
+  let run = testCase.testRun!
+
+  expectEqual(1, run.testCaseCount)
+  expectEqual(1, run.executionCount)
+  expectEqual(1, run.failureCount)
+  expectEqual(0, run.unexpectedExceptionCount)
+  expectEqual(1, run.totalFailureCount)
+  expectFalse(run.hasSucceeded)
 }
+
+runAllTests()
 
